a930df04d4c4855438b68a5101e120a5e05b2a03
Chetan <chetan@equaljustice.ai>
2024-03-02 17:40:06 +0530

    Leagla training and pramaters updated'

--- chatGPT/completion.js
+++ chatGPT/completion.js
@@ -14,11 +14,13 @@ async function createMessageContent(prompttype, userInputData, threadId) {
     },
     {
       "role": "user",
-      "content": `${userInputPara}
-${legalTraining} Generate a comprehensive, well-structured letter incorporating all the above elements, tailored to the specific details provided and aligned with the legal guidelines. The letter should be professional, convincing, and geared towards achieving a positive outcome.`
+      "content": `User's inputs and response is given below:
+      ${userInputPara}
+      Guiding RBI guidelines and legal framework is given below:
+      ${legalTraining}`
     }
   ]
-  processDocx(message[0].content + '\n' + message[1].content, threadId, constants.USERINPUTFILENAME + prompttype);
+  processDocx(message[0].content + '\n' + message[1].content, threadId, threadId + constants.USERINPUTFILENAME + prompttype);
   return message;
 }
 export async function createLetterwithFineTuned(prompttype, userInputData, threadId) {
@@ -26,9 +28,10 @@ export async function createLetterwithFineTuned(prompttype, userInputData, threa
 
     message = await createMessageContent(prompttype, userInputData, threadId);
     const FineTunedModelResponse = await openAiCompletionWithFineTunedATMBank(message);
-    processDocx(JSON.stringify(FineTunedModelResponse, null, 2), threadId, constants.FINE_TUNED_RESPONSE_JSON + prompttype);
-    processDocx(FineTunedModelResponse.choices[0].message.content, threadId, constants.GPT3_5_FINE_TUNED + prompttype);
-    return ;
+    processDocx(FineTunedModelResponse.choices[0].message.content, threadId, threadId + constants.GPT3_5_FINE_TUNED + prompttype);
+    // processDocx(JSON.stringify(FineTunedModelResponse, null, 2), threadId, threadId+constants.FINE_TUNED_RESPONSE_JSON + prompttype);
+
+    return;
   }
   catch (error) {
     error
@@ -38,11 +41,11 @@ export async function createLetterwithFineTuned(prompttype, userInputData, threa
 export async function createLetterWithGPT3_5(prompttype, userInputData, threadId) {
   try {
 
-    message = message!=null? message: await createMessageContent(prompttype, userInputData, threadId);
+    message = message != null ? message : await createMessageContent(prompttype, userInputData, threadId);
     const GPT3_5Response = await openAiCompletionWithGPT3_5(message);
-    processDocx(JSON.stringify(GPT3_5Response, null, 2), threadId, constants.GPT3_5_RESPONSE_JSON + prompttype);
-    processDocx(GPT3_5Response.choices[0].message.content, threadId, constants.GPT3_5 + prompttype);
-    return ;
+    // processDocx(JSON.stringify(GPT3_5Response, null, 2), threadId, threadId+constants.GPT3_5_RESPONSE_JSON + prompttype);
+    processDocx(GPT3_5Response.choices[0].message.content, threadId, threadId + constants.GPT3_5 + prompttype);
+    return;
   }
   catch (error) {
     error
@@ -52,81 +55,120 @@ export async function createLetterWithGPT3_5(prompttype, userInputData, threadId
 export async function createUserInputParagraph(userInputData, threadId) {
   try {
 
+    let updatedUserData = await removeKeys(userInputData);
     let userInputMessage = [
       {
         "role": "system",
-        "content": `Your Job is to convert the given Json object in fact based paragraph  from the perspective of the victim which can be read by a LLM model`
+        "content": `Your Job is to convert the given Json object in fact based paragraph from the perspective of the victim which can be read by a LLM model`
       },
       {
         "role": "user",
-        "content": `Given the following details about a financial fraud incident, generate a fact-based paragraph from the perspective of the me:
-        ${JSON.stringify(userInputData, null, 2)}`
+        "content": `Given the following details about a financial fraud incident, generate a fact-based paragraph from the perspective of me:
+        ${JSON.stringify(updatedUserData, null, 2)}`
       }
     ];
-    const GPT3_5Response = await openAiCompletionWithGPT3_5(userInputMessage);
-    processDocx(JSON.stringify(GPT3_5Response, null, 2), threadId, constants.GPT3_5_RESPONSE_JSON + 'UserInputPara');
-    return GPT3_5Response.choices[0].message.content;
+    /* console.log(`JSON: ${JSON.stringify(userInputData, null, 2)}
+    JsonAfterRemovedKeys: ${JSON.stringify(updatedUserData, null, 2)})`); */
+      const GPT3_5Response = await openAiCompletionWithGPT3_5(userInputMessage, 0.8, 0.8);
+      processDocx(`JSON: ${JSON.stringify(userInputData, null, 2)}
+      JsonAfterRemovedKeys: ${JSON.stringify(updatedUserData, null, 2)}
+      Paragraph: ${JSON.stringify(GPT3_5Response, null, 2)}`, threadId, threadId+constants.GPT3_5_RESPONSE_JSON + 'UserInputPara');
+     return GPT3_5Response.choices[0].message.content; 
   }
   catch (error) {
     error
   }
 }
+
+async function removeKeys(jsonData) {
+  let cleanedJson = JSON.parse(JSON.stringify(jsonData)); // Create a deep copy of the json
+  (function _clean(obj) {
+    if (obj && typeof obj === 'object') {
+      if (Array.isArray(obj)) {
+        obj.forEach((item, index) => {
+          obj[index] = _clean(item);
+        });
+      } else {
+        Object.keys(obj).forEach(key => {
+          if (obj[key] && typeof obj[key] === 'object') {
+            obj[key] = _clean(obj[key]); // Recurse if the value is an object
+          } else if (['Senior_citizen', 'pension_savings_account',
+          'withdrawn_amount_exceed_daily_limit', 'transaction_happen_after_informed_bank_of_previous_fraud']
+          .includes(key) && String(obj[key]).toLowerCase() === 'no') {
+            delete obj[key];
+          } else if (['applied_for_atmcard', 'withdrawing_regularly_from_atm',
+          'refund_compensesion_expected',
+          'transaction_sms_recieved', 'transaction_sms_recieved_within_one_hour',
+          'transaction_email_recieved', 'transaction_email_recieved_within_one_hour',
+          'transaction_from_ATM_in_home_city_or_work_city',
+          'transaction_from_ATM_in_home_city_or_work_city_regularly_withdrawing']
+          .includes(key) && String(obj[key]).toLowerCase() === 'yes') {
+            delete obj[key]; // Delete the key if the value is 'yes' 
+          }
+        });
+      }
+    }
+    return obj;
+  })(cleanedJson);
+  return cleanedJson;
+}
+
 export async function createLetterWithGPT4(prompttype, userInputData, threadId) {
   try {
 
-    message = message!=null? message: await createMessageContent(prompttype, userInputData, threadId);
+    message = message != null ? message : await createMessageContent(prompttype, userInputData, threadId);
     const GPT4Response = await openAiCompletionWithGPT4(message);
-    processDocx(JSON.stringify(GPT4Response, null, 2), threadId, constants.GPT4_RESPONSE_JSON + prompttype);
-    processDocx(GPT4Response.choices[0].message.content, threadId, constants.GPT4 + prompttype);
-    return ;
+    // processDocx(JSON.stringify(GPT4Response, null, 2), threadId, threadId+constants.GPT4_RESPONSE_JSON + prompttype);
+    processDocx(GPT4Response.choices[0].message.content, threadId, threadId + constants.GPT4 + prompttype);
+    return;
   }
   catch (error) {
     error
   }
 }
 
-async function openAiCompletionWithFineTunedATMBank(message) {
+async function openAiCompletionWithFineTunedATMBank(message, temperature = 0.5, top_p = 0.9, frequency_penalty = 0.2, presence_penalty = 0) {
   const openai = new OpenAI(process.env.OPENAI_API_KEY);
   //console.log("input to openAI", message);
   const completionResponse = await openai.chat.completions.create({
     model: "ft:gpt-3.5-turbo-0613:personal::8twTZDyP",
     messages: message,
-    temperature: 0.5,
+    temperature: temperature,
     max_tokens: 1500,
     n: 1,
-    top_p: 1,
-    frequency_penalty: 1.07,
-    presence_penalty: 0,
+    top_p: top_p,
+    frequency_penalty: frequency_penalty,
+    presence_penalty: presence_penalty,
   });
   return completionResponse;
 }
-async function openAiCompletionWithGPT3_5(message) {
+async function openAiCompletionWithGPT3_5(message, temperature = 0.5, top_p = 0.9, frequency_penalty = 0.2, presence_penalty = 0) {
   const openai = new OpenAI(process.env.OPENAI_API_KEY);
   const completionResponse = await openai.chat.completions.create({
     model: "gpt-3.5-turbo-0613",
     messages: message,
-    temperature: 0.5,
+    temperature: temperature,
     max_tokens: 1500,
     n: 1,
-    top_p: 0.9,
-    frequency_penalty: 0.2,
-    presence_penalty: 0,
+    top_p: top_p,
+    frequency_penalty: frequency_penalty,
+    presence_penalty: presence_penalty,
   });
   return completionResponse;
 }
-async function openAiCompletionWithGPT4(message) {
+async function openAiCompletionWithGPT4(message, temperature = 0.5, top_p = 1, frequency_penalty = 1.07, presence_penalty = 0) {
   const openai = new OpenAI(process.env.OPENAI_API_KEY);
   const completionResponse = await openai.chat.completions.create({
     model: "gpt-4-0125-preview",
     messages: message,
-    temperature: 0.5,
+    temperature: temperature,
     max_tokens: 1500,
     n: 1,
-    top_p: 1,
-    frequency_penalty: 1.07,
-    presence_penalty: 0,
+    top_p: top_p,
+    frequency_penalty: frequency_penalty,
+    presence_penalty: presence_penalty,
   });
- return completionResponse;
+  return completionResponse;
 }
 async function getLegalTrainingMaterials(userInputData) {
   const result = [];
