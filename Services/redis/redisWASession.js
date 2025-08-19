feeb24fb4595ea56dd8af0414b9750f04f9d5fdd
Prashank_quapton <prashank.kaushik@quapton.com>
2025-08-20 01:30:52 +0530

    Url change to new version

--- Services/redis/redisWASession.js
+++ Services/redis/redisWASession.js
@@ -7,7 +7,7 @@ const client = createClient({
     //     host: process.env.Redis_host,
     //     port: 11212
     // }
-    url: process.env.Redis_url_custom
+    url: process.env.Redis_url_upstash_custom
 });
 try {
     // Connect the client
